d63a1377146cd0c4a14d5e3d80ec18bb3d3c6ea7
Weston Knowlton <kno18012@byui.edu>
2021-04-24 14:57:50 -0600

    week1 final

--- js/main.js
+++ js/main.js
@@ -5,14 +5,17 @@ const links = [
     }
 ];
 
-links.forEach(link => {
-    let li = document.createElement('li');
-
-    let a = document.createElement('a');
-    a.setAttribute('href', link.url);
-    a.textContent = link.label;
-
-    li.appendChild(a);
-
-    document.querySelector('links').appendChild(li);
+window.addEventListener('load', (event) =>{
+    links.forEach(link => {
+        let li = document.createElement('li');
+    
+        let a = document.createElement('a');
+        a.setAttribute('href', link.url);
+        a.textContent = link.label;
+    
+        li.appendChild(a);
+    
+        document.getElementById('links').appendChild(li);
+    });
 });
+
