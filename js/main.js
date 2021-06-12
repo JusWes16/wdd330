7b72355056c4885f05bade274b3be5ee289047f2
Weston Knowlton <kno18012@byui.edu>
2021-06-12 05:16:38 -0600

    links

--- js/main.js
+++ js/main.js
@@ -1,37 +1,72 @@
 const links = [
     {
         label: "Week 1",
-        url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w01/index.html"
+        url: "https://juswes16.github.io/wdd330/w01/index.html"
     },
     {
         label: "Week 2",
-        url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w02/index.html"
+        url: "https://juswes16.github.io/wdd330/w02/index.html"
     },
     {
         label: "Week 3",
-        url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w03/index.html"
+        url: "https://juswes16.github.io/wdd330/w03/index.html"
     },
     {
         label: "Week 4",
-        url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w04/index.html"
+        url: "https://juswes16.github.io/wdd330/w04/index.html"
     },
     {
         label: "Week 5",
-        url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w05/index.html"
+        url: "https://juswes16.github.io/wdd330/w05/index.html"
     },
     {
         label: "Week 6",
-        url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w06/index.html"
+        url: "https://juswes16.github.io/wdd330/w06/index.html"
     },
     {
         label: "Week 7",
-        url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w07/index.html"
+        url: "https://juswes16.github.io/wdd330/w07/index.html"
     },
     {
         label: "Week 8",
-        url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w08/index.html"
+        url: "https://juswes16.github.io/wdd330/w08/index.html"
     }
 ];
+
+// const links = [
+//     {
+//         label: "Week 1",
+//         url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w01/index.html"
+//     },
+//     {
+//         label: "Week 2",
+//         url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w02/index.html"
+//     },
+//     {
+//         label: "Week 3",
+//         url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w03/index.html"
+//     },
+//     {
+//         label: "Week 4",
+//         url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w04/index.html"
+//     },
+//     {
+//         label: "Week 5",
+//         url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w05/index.html"
+//     },
+//     {
+//         label: "Week 6",
+//         url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w06/index.html"
+//     },
+//     {
+//         label: "Week 7",
+//         url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w07/index.html"
+//     },
+//     {
+//         label: "Week 8",
+//         url: "C:/Users/Gaming PC/Documents/College/Spring 2021/wdd330/wdd330/w08/index.html"
+//     }
+// ];
 
 window.addEventListener('load', (event) =>{
     links.forEach(link => {
